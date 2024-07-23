b34bca1dc247b43846efd2f77597d06b333f16d4
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-07-23 08:51:31 -0500

    Create load_screenplay.php

---
+++ Web/New files/load_screenplay.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "sql201.infinityfree.com";
+$username = "if0_36946962";
+$password = "your_password";
+$dbname = "if0_36946962_screenplay_website";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+
+$sql = "SELECT screenplays.content FROM screenplays JOIN users ON screenplays.user_id = users.id WHERE users.email='$email' LIMIT 1";
+$result = $conn->query($sql);
