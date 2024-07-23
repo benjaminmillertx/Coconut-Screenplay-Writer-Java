a967a63493cee525ddaa835dc3398714547690f6
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-07-22 20:23:08 -0500

    Create footer.php

---
+++ Web/includes/footer.php
@@ -0,0 +1,8 @@
+    </main>
+    <footer>
+        <p>&copy; 2024 Screenplay Writing Website</p>
+    </footer>
+    <script src="/public/js/shortcuts.js"></script>
+    <script src="/public/js/spellcheck.js"></script>
+</body>
+</html>
