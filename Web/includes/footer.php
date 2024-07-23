67e7e46b4feda4dfc52207e0b929b9fc842a440b
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-07-22 21:00:04 -0500

    Update footer.php

--- Web/includes/footer.php
+++ Web/includes/footer.php
@@ -2,6 +2,7 @@
     <footer>
         <p>&copy; 2024 Screenplay Writing Website</p>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/public/js/shortcuts.js"></script>
     <script src="/public/js/spellcheck.js"></script>
 </body>
